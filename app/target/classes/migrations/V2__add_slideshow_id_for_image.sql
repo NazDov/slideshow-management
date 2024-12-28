a28196f97af0d1ad27f617e98b50a458bbdfb71a
Nazar Dovhyy <nazardovhuy@gmail.com>
2024-12-28 22:48:06 +0200

    remove bin files and add gitignore

--- app/target/classes/migrations/V2__add_slideshow_id_for_image.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE image
-    ADD COLUMN slideshow_id BIGINT DEFAULT NULL;
-
--- rollaback
--- ALTER TABLE image DROP COLUMN slideshow_id
--- app/target/classes/migrations/V3__add_slideshow_table.sql
+++
@@ -1,7 +0,0 @@
--- Slideshow Table
-CREATE TABLE IF NOT EXISTS slideshow (
-                                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                         name VARCHAR(50) DEFAULT NULL,
-                                         is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-                                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
